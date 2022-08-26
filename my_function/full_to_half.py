f1bce042eb77955caec50700170357fa2a9a6cfb
tazz7420 <xinzhe458@gmail.com>
2022-08-26 14:20:46 +0800

    20220826 update

--- my_function/full_to_half.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def h2f(dataseries):
-    full_cha = ['０', '１','２','３','４','５','６','７','８','９']
-    num = 0
-    for c in full_cha:
-        dataseries = dataseries.str.replace(str(c),str(num))
-        num = num + 1
-    return dataseries
